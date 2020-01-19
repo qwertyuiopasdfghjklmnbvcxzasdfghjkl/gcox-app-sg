42ac245c9c392bc4a6d06bd99210740411eece32
luckYang <574748221@qq.com>
2020-01-19 12:27:38 +0800

    新国际化

--- src/views/dialog/selectWeb.vue
+++ src/views/dialog/selectWeb.vue
@@ -3,7 +3,7 @@
         <!--<h4 class="title">{{vm.$t('user.area_chioce')}}</h4>-->
         <h2>{{vm.$t('user.goto_main_site')}}</h2>
         <small>{{vm.$t('user.curr_pos_notsg')}}</small>
-        <p>{{vm.$t('user.goto_desc')}}</p>
+        <p>{{vm.$t('user.goto_desc_com')}}</p>
         <p class="btn next" @click="com()">{{vm.$t('shop.continue')}}</p>
         <p class="btn" @click="sg()">{{vm.$t('user.curr_stay')}}</p>
     </div>
